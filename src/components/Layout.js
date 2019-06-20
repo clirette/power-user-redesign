34afd09283eaa53428bd6ac7db0397eee4035635
Chase Lirette <chaselirette@yahoo.com>
2019-06-20 17:21:39 -0500

    Giving up on rerender for now.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
-import React, { Fragment, useState, useEffect } from "react"
-import { Link } from "gatsby"
+import React, { Fragment } from "react"
 import { Helmet } from "react-helmet"
-import styled, { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components"
 import { Normalize } from "styled-normalize"
 
 import Navbar from "./Navbar"
@@ -18,38 +17,26 @@ const GlobalStyle = createGlobalStyle`
 `
 
 const Layout = props => {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    setIsLoading(false)
-  })
-
   return (
     <Fragment>
-      {isLoading ? (
-        "Loading..."
-      ) : (
-        <Fragment>
-          <Helmet>
-            <meta charSet="utf-8" />
-            <title>
-              Power User{props.pageTitle ? ` | ${props.pageTitle}` : ""}
-            </title>
-            <link
-              href="https://fonts.googleapis.com/css?family=Montserrat:400,500&display=swap"
-              rel="stylesheet"
-            />
-            <meta
-              name="viewport"
-              content="width=device-width,height=device-height,initial-scale=1.0"
-            />
-          </Helmet>
-          <GlobalStyle />
-          <Normalize />
-          <Navbar />
-          {props.children}
-        </Fragment>
-      )}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>
+          Power User{props.pageTitle ? ` | ${props.pageTitle}` : ""}
+        </title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:400,500&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,height=device-height,initial-scale=1.0"
+        />
+      </Helmet>
+      <GlobalStyle />
+      <Normalize />
+      <Navbar />
+      {props.children}
     </Fragment>
   )
 }

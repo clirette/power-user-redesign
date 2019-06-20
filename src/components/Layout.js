5c4bf0252e0f90b496f5770ca87d031da47b34dd
Chase Lirette <clirette@dxc.com>
2019-06-20 14:46:42 -0500

    Using Gatsby hello-world starter for power-user redesign. Implemented responsive Navbar.

---
+++ src/components/Layout.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from "react"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+import styled, { createGlobalStyle } from "styled-components"
+import { Normalize } from "styled-normalize"
+
+import Navbar from "./Navbar"
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+`
+
+const Layout = props => {
+  return (
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Power User</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:400,500&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,height=device-height,initial-scale=1.0"
+        />
+      </Helmet>
+      <GlobalStyle />
+      <Normalize />
+      <Navbar />
+      {props.children}
+    </Fragment>
+  )
+}
+
+export default Layout
---
+++ src/components/Navbar.js
@@ -0,0 +1,157 @@
+import React, { Component, Fragment } from "react"
+import { Link as GatsbyLink } from "gatsby"
+import styled from "styled-components"
+
+import devices from "../utils/devices"
+
+const StyledNavbar = styled.div`
+  height: 10vh;
+  background-color: #660066;
+  display: flex;
+  color: #fff;
+`
+const BrandContainer = styled.div`
+  flex: 2;
+  align-self: center;
+  @media ${devices.laptop} {
+    flex: 1;
+  }
+`
+const Hamburger = styled.div`
+  height: 2rem;
+  width: 2rem;
+  border: 0.2rem solid #fff;
+  border-radius: 0.1rem;
+  display: none;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  align-self: center;
+  margin-right: 2rem;
+  cursor: pointer;
+  @media ${devices.mobileL} {
+    display: flex;
+  }
+`
+
+const HamburgerLines = styled.hr`
+  width: 50%;
+  color: #fff;
+  margin: 0;
+`
+
+const Link = styled(GatsbyLink)`
+  font-size: 2rem;
+  color: #ddd;
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+  @media (hover: hover) {
+    &:hover {
+      color: #fff;
+    }
+  }
+`
+
+const Brand = styled.h1`
+  font-size: 2.6rem;
+  font-weight: 500;
+  letter-spacing: 0.3rem;
+  margin-left: 2rem;
+  color: #fff;
+`
+
+const NavLinksContainer = styled.nav`
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  @media ${devices.mobileL} {
+    display: none;
+  }
+`
+
+const active = {
+  color: "#fff",
+  borderBottom: ".2rem solid #fff",
+}
+
+const collapsedNavContainerStyles = {
+  display: "flex",
+  backgroundColor: "#660066",
+  flexDirection: "column",
+  alignItems: "end",
+  paddingLeft: "1rem",
+  active: { color: "#fff" },
+  link: { paddingTop: ".5rem", paddingBottom: ".5rem", fontSize: "1.8rem" },
+}
+
+const NavLinks = props => (
+  <NavLinksContainer style={props.display}>
+    <Link
+      activeStyle={props.display ? props.display.active : active}
+      to="/"
+      style={props.display ? props.display.link : {}}
+    >
+      Home
+    </Link>
+    <Link
+      activeStyle={props.display ? props.display.active : active}
+      to="/about"
+      style={props.display ? props.display.link : {}}
+    >
+      About
+    </Link>
+    <Link
+      activeStyle={props.display ? props.display.active : active}
+      to="/services"
+      style={props.display ? props.display.link : {}}
+    >
+      Services
+    </Link>
+    <Link
+      activeStyle={props.display ? props.display.active : active}
+      to="/contact"
+      style={props.display ? props.display.link : {}}
+    >
+      Contact
+    </Link>
+  </NavLinksContainer>
+)
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { menuExpanded: false }
+  }
+
+  handleHamburgerClick = e => {}
+
+  render() {
+    return (
+      <Fragment>
+        <StyledNavbar>
+          <BrandContainer>
+            <Link to="/">
+              <Brand>Power User</Brand>
+            </Link>
+          </BrandContainer>
+          <Hamburger
+            onClick={() =>
+              this.setState({ menuExpanded: !this.state.menuExpanded })
+            }
+          >
+            <HamburgerLines></HamburgerLines>
+            <HamburgerLines></HamburgerLines>
+            <HamburgerLines></HamburgerLines>
+          </Hamburger>
+          {!this.state.menuExpanded && <NavLinks />}
+        </StyledNavbar>
+        {this.state.menuExpanded && (
+          <NavLinks display={collapsedNavContainerStyles} />
+        )}
+      </Fragment>
+    )
+  }
+}
+
+export default Navbar
